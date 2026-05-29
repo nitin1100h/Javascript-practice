9b3ede247eb7290f55caacf76b40c83a8c6bf308
nitin1100h <nitinjha2069@gmail.com>
2026-05-30 00:06:43 +0530

    this is the push from vs code

---
+++ basics/string_to _number.js
@@ -0,0 +1,35 @@
+// let value=3
+// let negvalue=-value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+// Basic concept of joining 2 different strings
+// let str1="hello "
+// let str2="world"
+// let str3=str1+str2;
+// console.log(str3);
+
+// console.log(1+"2");//output=12
+// console.log("1"+2);//output=12
+// console.log("1"+2+2);//output=122
+// console.log(1+2+"2");//output=32
+
+// There is a very answer for this behaviour of js but we just have to keep in mind to use parenthesis for cleaner and easy to understand code.
+// Just keep in mind the basic thing if the first element is string it will be treated as string or if the first and second elemnet is integer it will be treated as integer.
+
+console.log(true);//output=true
+console.log(+true);//output=1
+//console.log(true+);//output= Error
+console.log(+"");//output=0
+
+//Keep in mind these types of codes is not at all entertained in the corporate world.
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
